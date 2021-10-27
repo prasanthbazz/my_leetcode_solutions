b5707e9724da3f268aebd1dd3ceda154e06c7f1d
Prasanth B <prasanthtvm43@gmail.com>
2021-10-28 00:14:47 +0530

    solved daily challenge - 28/10/21

---
+++ src/solutions/python/sort-colors.py
@@ -0,0 +1,24 @@
+'''
+#75
+https://leetcode.com/problems/sort-colors/
+'''
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+        r = len(nums) - 1
+        i = 0
+        while(i <= r):
+            if nums[i] == 0:
+                nums[i], nums[l] = nums[l], nums[i]
+                l += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[i], nums[r] = nums[r], nums[i]
+                r -= 1
+            else:
+                i += 1
+        return nums
